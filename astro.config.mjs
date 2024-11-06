0eba58051c14b10b04238f8bb62c3597eae13311
Thamsanqa Dreem <thamsanqa.dev@gmail.com>
2024-11-06 19:11:58 +0200

    added vercel adapter and favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
 
+import vercel from "@astrojs/vercel/serverless";
+
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
+  adapter: vercel(),
 });
